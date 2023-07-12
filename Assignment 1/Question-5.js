e78ec12abf86bcbbaab9eab8b0894c80dc839181
Sourabh Bhatt <sourabhbhatt825@gmail.com>
2023-07-12 18:31:17 +0530

    Q-5 Completed

---
+++ Assignment 1/Question-5.js
@@ -0,0 +1,23 @@
+function mergeSort(nums1, m, nums2, n) {
+  let i = m - 1;
+  let j = n - 1;
+  let k = m + n - 1;
+
+  while (i >= 0 && j >= 0) {
+    if (nums1[i] > nums2[j]) {
+      nums1[k--] = nums1[i--];
+    } else {
+      nums1[k--] = nums2[j--];
+    }
+  }
+
+  while (j >= 0) {
+    nums1[k--] = nums2[j--];
+  }
+}
+const nums1 = [1, 2, 3, 0, 0, 0];
+const m = 3;
+const nums2 = [2, 5, 6];
+const n = 3;
+mergeSort(nums1, m, nums2, n);
+console.log(nums1);
