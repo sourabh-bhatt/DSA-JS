c228d89f1501c90eb9c7d786a9d129d98bca3617
Sourabh Bhatt <sourabhbhatt825@gmail.com>
2023-07-12 18:24:14 +0530

    Q-3 Completed

---
+++ Assignment 1/Question-3.js
@@ -0,0 +1,23 @@
+function binarySearch(nums, target) {
+  let left = 0;
+  let right = nums.length - 1;
+
+  while (left <= right) {
+    let mid = Math.floor((left + right) / 2);
+
+    if (nums[mid] === target) {
+      return mid;
+    } else if (nums[mid] < target) {
+      left = mid + 1;
+    } else {
+      right = mid - 1;
+    }
+  }
+
+  return left;
+}
+const nums = [1, 3, 5, 6];
+const target = 5;
+
+const result = binarySearch(nums, target);
+console.log("Output:", result);
